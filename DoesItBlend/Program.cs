f99876af59f414b9d3e90dd3cc02b856cf092f54
milan <milan@CIS-RDS2>
2019-09-20 14:30:03 -0500

    Added string output in Program.cs. Add IBlendable interface. Allowed all classes to work together.

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace DoesItBlend
 {
@@ -6,7 +8,23 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> blendables = new List<IBlendable>();
+            blendables.Add(new Strawberry());
+            blendables.Add(new Banana());
+            blendables.Add(new CellPhone());
+            blendables.Add(new IceCubes());
+            for (int i = 0; i < 10; i++)
+                blendables.Add(new Mango());
+
+            blendables.Add(new Banana());
+            Console.WriteLine(new Banana().Blend());
+
+            StringBuilder mess = new StringBuilder();
+            foreach (IBlendable blendable in blendables)
+            {
+                mess.Append(blendable.Blend());
+            }
+            Console.WriteLine(mess.ToString());
         }
     }
 }
